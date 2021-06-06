161f49a09aabc27259cd6fb28ea7ad68f3b97c29
roknabadi <74315203+roknabadi@users.noreply.github.com>
2021-06-06 16:29:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import simplejson as json
 from datetime import datetime
+import datetime
 import streamlit as st
 import altair as alt
 import pandas as pd
@@ -13,19 +14,29 @@ key = os.getenv("key")
 
 
 ticker=st.text_input("Ticker symbol: ", 'AAPL')
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
+start_date=st.sidebar.date_input('start date', datetime.date(2011,1,1))
+end_date=st.sidebar.date_input('end date', datetime.date(2021,1,1))
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}&outputsize=full'.format(ticker, key)
 response = requests.get(url)
 if 'Error Message' in response.json():
     st.text(response.json()['Error Message'])
 else:
-    data=response.json()['Time Series (Daily)']
-    data= {key: data[key]['5. adjusted close'] for key in sorted(data.keys())}
-    x, y = zip(*sorted(data.items()))
-    y = [float(i) for i in y]
-    temp = pd.DataFrame({'Date':x, 'Price':y})
-    temp['Date']=pd.to_datetime(temp['Date'])
-    title = 'Historical Adjusted Closing Price for '+ticker
-    c= alt.Chart(temp).mark_line().encode(
-        x='Date',
-        y='Price').properties(title=title)
-    st.write(c)
+    if start_date < end_date:
+            data=response.json()['Time Series (Daily)']
+            data= {key: data[key]['5. adjusted close'] for key in sorted(data.keys())}
+            x, y = zip(*sorted(data.items()))
+            y = [float(i) for i in y]
+            temp = pd.DataFrame({'Date':x, 'Price':y})
+            temp['Date']=pd.to_datetime(temp['Date'])
+            temp=temp[(temp['Date']>pd.to_datetime(start_date))&(temp['Date']<pd.to_datetime(end_date))]
+            title = 'Historical Adjusted Closing Price for '+ticker
+            c= alt.Chart(temp).mark_line().encode(
+                x='Date',
+                y='Price').properties(title=title)
+            st.write(c)
+    else:
+            st.text('Start date is not before the end date!')
+
+
+
+
